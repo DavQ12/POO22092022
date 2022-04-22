24be83ba2df166eb8e882f7b053bd041cc91d2eb
DavQ12 <angelquintero63@aragon.unam.mx>
2022-04-21 21:24:22 -0500

    Tarea: Cuentahabiente

---
+++ tareas/Cuentahabiente/src/cuentahabiente/Cuentahabiente.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuentahabiente;
+
+/**
+ *
+ * @author Angel
+ */
+public class Cuentahabiente {
+    private int idCliente;
+    private String nombre;
+    private float balance;
+
+    public Cuentahabiente() {
+    }
+
+    public Cuentahabiente(int idCliente, String nombre, float balance) {
+        this.idCliente = idCliente;
+        this.nombre = nombre;
+        this.balance = balance;
+    }
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Cuentahabiente{" + "idCliente=" + idCliente + ", nombre=" + nombre + ", balance=" + balance + '}';
+    }
+    
+    public void retirarDinero(float monto){
+        this.balance -= monto;
+        System.out.println("Se hizo un retiro de: "+ monto+ "$. Ahora su monto es de: " +this.balance+"$.");
+    }
+    
+    public String evaluarCliente(){
+        return this.balance >50000? "Cliente premium":"Cliente regular";
+    }
+    
+    
+}
